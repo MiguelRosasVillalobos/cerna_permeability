2c392a1876b568425c07e2b4ac6dd031dadbb35c
MiguelRosasVillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-03-27 22:46:39 -0300

    Se parametriza todo lo modificable

--- start_simulation.sh
+++ start_simulation.sh
@@ -9,6 +9,48 @@ fi
 
 # Obtiene la cantidad desde el primer argumento
 cantidad=$1
+# Leer valores desde el archivo parametros.txt
+nu=$(grep -oP 'nu\s*=\s*\K[\d.+-]+' parametros.txt)
+d=$(grep -oP 'd\s*=\s*\K[\d.+-]+' parametros.txt)
+Re=$(grep -oP 'Re\s*=\s*\K[\d.+-]+' parametros.txt)
+
+lc=$(grep -oP 'lc\s*=\s*\K[\d.+-]+' parametros.txt)
+lcc=$(grep -oP 'lcc\s*=\s*\K[\d.+-]+' parametros.txt)
+rd=$(grep -oP 'rd\s*=\s*\K[\d.+-]+' parametros.txt)
+l1=$(grep -oP 'l1\s*=\s*\K[\d.+-]+' parametros.txt)
+a=$(grep -oP 'a\s*=\s*\K[\d.+-]+' parametros.txt)
+rp=$(grep -oP 'rp\s*=\s*\K[\d.+-]+' parametros.txt)
+np=$(grep -oP 'np\s*=\s*\K[\d.+-]+' parametros.txt)
+
+tf=$(grep -oP 'tf\s*=\s*\K[\d.+-]+' parametros.txt)
+dt=$(grep -oP 'dt\s*=\s*\K[\d.+-]+' parametros.txt)
+wi=$(grep -oP 'wi\s*=\s*\K[\d.+-]+' parametros.txt)
+
+# Reemplazar valores en sus respectivos archivos
+sed -i "s/\$nu/$nuu/g" ./Case_0/0/U
+sed -i "s/\$nu/$nuu/g" ./Case_0/constant/transportProperties
+sed -i "s/\$Re/$Ree/g" ./Case_0/0/U
+sed -i "s/\$d/$dd/g" ./Case_0/0/U
+
+sed -i "s/\$lc/$lccc/g" ./Case_0/mesh.geo
+sed -i "s/\$rd/$rdd/g" ./Case_0/mesh.geo
+sed -i "s/\$l1/$l11/g" ./Case_0/mesh.geo
+sed -i "s/\$a/$aa/g" ./Case_0/mesh.geo
+sed -i "s/\$lcc/$lcccc/g" ./Case_0/mesh.geo
+
+sed -i "s/\$lc/$lccc/g" ./Case_0/geometry_script/geometry.geo
+sed -i "s/\$rd/$rdd/g" ./Case_0/geometry_script/geometry.geo
+sed -i "s/\$l1/$l11/g" ./Case_0/geometry_script/geometry.geo
+sed -i "s/\$a/$aa/g" ./Case_0/geometry_script/geometry.geo
+sed -i "s/\$rp/$rpp/g" ./Case_0/geometry_script/geometry.geo
+
+sed -i "s/\$np/$npp/g" ./Case_0/geometry_script/generator_point_process.py
+sed -i "s/\$rp/$rpp/g" ./Case_0/geometry_script/generator_point_process.py
+sed -i "s/\$rd/$rdd/g" ./Case_0/geometry_script/generator_point_process.py
+
+sed -i "s/\$wi/$wii/g" ./Case_0/system/controlDict
+sed -i "s/\$dt/$dtt/g" ./Case_0/system/controlDict
+sed -i "s/\$tf/$tff/g" ./Case_0/system/controlDict
 
 # Bucle para crear y mover carpetas, editar y genrar mallado
 for ((i = 1; i <= $cantidad; i++)); do
